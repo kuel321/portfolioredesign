f38afd18f5cb24577de3a56314a98be34f6dbab7
lshort <lshort@bulldogcreative.com>
2022-12-28 16:46:38 -0500

    fixed footer and added day of week

--- src/components/body-components/Footer.js
+++ src/components/body-components/Footer.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { motion } from "framer-motion"
 
 export default function Footer() {
+
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const d = new Date();
+let day = weekday[d.getDay()];
   return (
     <motion.div animate={{opacity:1}} initial={{opacity: 0}} transition={{duration: 2}} className="footer-container">
 
      <div className='footer-copyright'>
-     ⓒ Luke Short, 2022
+     ⓒ Luke Short, 2022 - Hope you're having a great {day}!
      </div>
 
 
